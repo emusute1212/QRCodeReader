d1aca51282f9d45bfeb7aee05ad5c89e50314ea9
emusute <miyanishi1212@gmail.com>
2018-12-30 14:31:34 +0900

    feat(qrcode-app): 読み取った内容に応じてボタンが出力されるようにした。

--- app/src/main/java/com/yosuke/qrcodereader/reader/QrCodeReadActivity.kt
+++ app/src/main/java/com/yosuke/qrcodereader/reader/QrCodeReadActivity.kt
@@ -1,12 +1,18 @@
 package com.yosuke.qrcodereader.reader
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.CaptureActivity
+import com.yosuke.qrcodereader.R
+import com.yosuke.qrcodereader.databinding.ActivityQrCodeReadActivityBinding
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 
 class QrCodeReadActivity : DaggerAppCompatActivity() {
@@ -16,20 +22,32 @@ class QrCodeReadActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        IntentIntegrator(this).also {
-            it.setOrientationLocked(false)
-            it.captureActivity = CaptureActivityAnyOrientation::class.java
-            it.initiateScan()
+
+        val binding = DataBindingUtil.setContentView<ActivityQrCodeReadActivityBinding>(this, R.layout.activity_qr_code_read_activity)
+
+        val viewModel = ViewModelProviders.of(this, viewModelFactory).get(QrCodeReadViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        //画面回転やActivity再生時などはスキャン画面へ移行しない
+        if (savedInstanceState == null) {
+            IntentIntegrator(this).also {
+                it.setOrientationLocked(false)
+                it.captureActivity = CaptureActivityAnyOrientation::class.java
+                it.initiateScan()
+            }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
                 ?: return
-        if (result.contents == null) {
-            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+        if (result.contents != null) {
+            Toast.makeText(this, "Scanned: ${result.contents}", Toast.LENGTH_LONG).show()
+            ViewModelProviders.of(this, viewModelFactory).get(QrCodeReadViewModel::class.java).setReadText(result.contents)
         } else {
-            Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            finish()
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
         }
     }
 
