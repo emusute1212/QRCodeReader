5640757fd6fb277bcb4f4cc62ab8f41bed3d8519
emusute <miyanishi1212@gmail.com>
2018-12-30 18:09:59 +0900

    feat(qrcode-app): GeneretarActivityのonCreateでLayoutファイルと紐づけた

--- app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorActivity.kt
+++ app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorActivity.kt
@@ -1,14 +1,28 @@
 package com.yosuke.qrcodereader.generator
 
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import com.yosuke.qrcodereader.R
+import com.yosuke.qrcodereader.databinding.ActivityGeneratorBinding
 import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
 
 class GeneratorActivity : DaggerAppCompatActivity() {
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityGeneratorBinding>(this, R.layout.activity_generator)
+
+        val viewModel = ViewModelProviders.of(this, viewModelFactory).get(GeneratorViewModel::class.java)
+
+        binding.viewModel = viewModel
     }
 
     companion object {
