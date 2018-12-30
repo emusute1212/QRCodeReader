f84af952617359e13c090add606a746e709229fc
emusute <miyanishi1212@gmail.com>
2018-12-30 17:48:37 +0900

    chore(qrcode-app): Activityをアプリに追加

--- app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorActivity.kt
+++ app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorActivity.kt
@@ -1,6 +1,11 @@
 package com.yosuke.qrcodereader.generator
 
+import android.os.Bundle
 import dagger.android.support.DaggerAppCompatActivity
 
 class GeneratorActivity : DaggerAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
