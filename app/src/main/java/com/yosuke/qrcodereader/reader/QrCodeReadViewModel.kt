9486497219d01eebfebf82bbfa95339a786ab20e
emusute <miyanishi1212@gmail.com>
2018-12-30 15:07:44 +0900

    feat(qrcode-app): ブラウザで開くボタンを実装

--- app/src/main/java/com/yosuke/qrcodereader/reader/QrCodeReadViewModel.kt
+++ app/src/main/java/com/yosuke/qrcodereader/reader/QrCodeReadViewModel.kt
@@ -2,15 +2,22 @@ package com.yosuke.qrcodereader.reader
 
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
+import android.net.Uri
 import android.webkit.URLUtil
 import javax.inject.Inject
 
-class QrCodeReadViewModel @Inject constructor() : ViewModel() {
+class QrCodeReadViewModel @Inject constructor(
+        private val navigator: QrCodeReadNavigator
+) : ViewModel() {
 
     val readText = ObservableField<String>()
 
     fun setReadText(value: String) {
         readText.set(value)
+    }
+
+    fun onOpenBrowserButton() {
+        navigator.openBrowser(Uri.parse(readText.get()))
     }
 
     fun isUrl(target: String): Boolean {
