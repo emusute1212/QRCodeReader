caa0997ae5f03c960312d9298be54dbd3e4e2538
emusute <miyanishi1212@gmail.com>
2018-12-30 17:50:06 +0900

    chore(qrcode-app): ActivityのModuleを追加

---
+++ app/src/main/java/com/yosuke/qrcodereader/di/module/GeneratorActivityModule.kt
@@ -0,0 +1,24 @@
+package com.yosuke.qrcodereader.di.module
+
+import android.arch.lifecycle.ViewModel
+import com.yosuke.qrcodereader.di.ViewModelKey
+import com.yosuke.qrcodereader.generator.GeneratorActivity
+import com.yosuke.qrcodereader.generator.GeneratorViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
+
+@Module
+interface GeneratorActivityModule {
+    @ContributesAndroidInjector(modules = [GeneratorViewModelModule::class])
+    fun contoributeGeneratorActivity(): GeneratorActivity
+}
+
+@Module
+abstract class GeneratorViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(GeneratorViewModel::class)
+    abstract fun provideGeneratorViewModel(viewModel: GeneratorViewModel): ViewModel
+}
