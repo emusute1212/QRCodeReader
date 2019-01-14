42a9180294977e26656de5cabb31b1783b97b15f
emusute <miyanishi1212@gmail.com>
2019-01-14 21:56:34 +0900

    feat(qrcode-app): ViewModelでQRコードを持つようにした

--- app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorViewModel.kt
+++ app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorViewModel.kt
@@ -2,9 +2,12 @@ package com.yosuke.qrcodereader.generator
 
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
+import android.graphics.Bitmap
 import javax.inject.Inject
+
 
 class GeneratorViewModel @Inject constructor(
 ) : ViewModel() {
     val qrCodeStr = ObservableField<String>()
+    val qrCode = ObservableField<Bitmap>()
 }
