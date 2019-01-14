8cce2fe026f083ef2e19ae59c534d3953122df7a
emusute <miyanishi1212@gmail.com>
2019-01-14 21:59:10 +0900

    feat(qrcode-app): QRコードを生成できるように変更

--- app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorViewModel.kt
+++ app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorViewModel.kt
@@ -3,6 +3,13 @@ package com.yosuke.qrcodereader.generator
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import android.graphics.Bitmap
+import android.graphics.Color
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel.L
+import kotlinx.coroutines.experimental.launch
+import java.util.*
 import javax.inject.Inject
 
 
@@ -10,4 +17,40 @@ class GeneratorViewModel @Inject constructor(
 ) : ViewModel() {
     val qrCodeStr = ObservableField<String>()
     val qrCode = ObservableField<Bitmap>()
+
+    fun onGenerateButton() {
+        if (qrCodeStr.get().isNullOrEmpty()) return
+        launch { qrCode.set(encode()) }
+        navigator.viewQrCodeDialog()
+    }
+    //TODO:エンコードは別の層で行う
+    private fun encode(): Bitmap {
+        //異なる型の値を入れるためgenericは使えない
+        val encodeHint = Hashtable<EncodeHintType, Any>()
+        encodeHint[EncodeHintType.CHARACTER_SET] = "UTF-8"
+
+        //エラー修復レベルを指定
+        //L 7%が復元可能
+        //M 15%が復元可能
+        //Q 25%が復元可能
+        //H 30%が復元可能
+        encodeHint[EncodeHintType.ERROR_CORRECTION] = L
+
+        // エンコード
+        val matrix = QRCodeWriter().encode(qrCodeStr.get(), BarcodeFormat.QR_CODE, 100, 100, encodeHint)
+        // ピクセルを作る
+        val width = matrix.width
+        val height = matrix.height
+        val pixels = IntArray(width * height)
+        // データがあるところだけ黒にする
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                pixels[(y * width) + x] = if (matrix.get(x, y)) Color.BLACK else Color.WHITE
+            }
+        }
+        return Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888).apply {
+            setPixels(pixels, 0, width, 0, 0, width, height)
+        }
+
+    }
 }
