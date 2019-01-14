783f0610760e41aad01a9058d7eebd011fb2a351
emusute <miyanishi1212@gmail.com>
2019-01-14 21:38:07 +0900

    feat(qrcode-app): qrコードにしたいテキストを双方向データバインディングできるようにした

--- app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorViewModel.kt
+++ app/src/main/java/com/yosuke/qrcodereader/generator/GeneratorViewModel.kt
@@ -1,8 +1,10 @@
 package com.yosuke.qrcodereader.generator
 
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
 import javax.inject.Inject
 
 class GeneratorViewModel @Inject constructor(
 ) : ViewModel() {
+    val qrCodeStr = ObservableField<String>()
 }
